8304ec045104e29132080d163e37fc9c517ea09d
Gonzalo Pardo <gonzalopardo.gep@gmail.com>
2022-10-01 22:45:12 -0300

    yo

---
+++ yo.js
@@ -0,0 +1,12 @@
+const yo = {
+    nombre: "gonzita",
+    edad: 24,
+    desarrollador: true,
+    nacimiento: new Date("january 18 1998"),
+    librofavorito: {
+        titulo: "ENCAMBIO",
+        autor: "Bachrach Estanislao",
+        fecha: new Date("september 2014"),
+        url: "https://www.cuspide.com/9789877252385/En+Cambio"
+    }
+}
